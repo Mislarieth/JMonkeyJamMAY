7f0a1133a8c4971c33bb6d66be95e527e614ad60
Jimmy Johnson <tonymastromt@gmail.com>
2014-05-23 20:34:19 -0400

    APPSTATES

---
+++ BlendswapArcade05-27-05/src/mygame/appstates/MainGame.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame.appstates;
+
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.scene.Node;
+
+/**
+ *
+ * @author User
+ */
+public class MainGame extends AbstractAppState{
+    private Node rootNode;
+    
+    
+}
